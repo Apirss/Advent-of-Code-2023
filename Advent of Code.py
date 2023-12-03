0d50331c540dd3785c6216ce8d1a520a12809d12
Chan Witzel Marco <marcochanwitzel@epita.fr>
2023-12-04 00:53:51 +0100

    J'abandonne ici mdr

--- Advent of Code.py
+++ Advent of Code.py
@@ -151,5 +151,129 @@ def day2_2(filename):
         id += 1
     return res
 
-print(day2_2("test.txt"))
-print(day2_2("input.txt"))
+# print(day2_2("test.txt"))
+# print(day2_2("input.txt"))
+
+def got_symbol(symbols, i, j, size_file, size_line):
+    if j - 1 >= 0:
+        if i-1 >= 0:
+            if symbols[i-1][j-1] or symbols[i-1][j]:
+                return True
+        if i+1 < size_file:
+            if symbols[i+1][j - 1] or symbols[i+1][j]:
+                return True
+        if symbols[i][j - 1]:
+            return True
+    if j + 1 < size_line:
+        if i - 1 >= 0:
+            if symbols[i - 1][j+1]:
+                return True
+        if i + 1 < size_file:
+            if symbols[i + 1][j+1]:
+                return True
+        if symbols[i][j +1]:
+            return True
+    return False
+def day3_1(filename):
+    file = open(filename, 'r')
+    lines = file.read().splitlines()
+    size_file = len(lines)
+    size_line = len(lines[0])
+    symbols = []
+    res = 0
+    for i in range(size_file):
+        symbols.append([])
+        for j in range(size_line):
+            if ('0' > lines[i][j] or lines[i][j] > '9') and lines[i][j] != '.':
+                symbols[i].append(True)
+            else:
+                symbols[i].append(False)
+
+    for i in range(size_file):
+        j = 0
+        while j < size_line:
+            if '0' <= lines[i][j] <= '9':
+                if j+1 < size_line and '0' <= lines[i][j+1] <= '9':
+                    if j + 2 < size_line and '0' <= lines[i][j + 2] <= '9':
+                        if got_symbol(symbols, i, j, size_file, size_line) or got_symbol(symbols, i, j+1, size_file, size_line) or got_symbol(symbols, i, j+2, size_file, size_line):
+                            num = int(lines[i][j] + lines[i][j+1] + lines[i][j + 2])
+                            res += num
+                        j += 3
+                    else:
+                        if got_symbol(symbols, i, j, size_file, size_line) or got_symbol(symbols, i, j+1, size_file, size_line):
+                            num = int(lines[i][j] + lines[i][j + 1])
+                            res += num
+                        j += 2
+                else:
+                    if got_symbol(symbols, i, j, size_file, size_line):
+                        num = int(lines[i][j])
+                        res += num
+                    j += 1
+            else:
+                j += 1
+
+    return res
+
+
+# print(day3_1("test.txt"))
+# print(day3_1("input.txt"))
+
+def got_num(lines, i, j, size_file, size_line):
+    stock = []
+    maxi = i+3
+    maxj = j+3
+    if i < 0:
+        i = 0
+    if j < 0:
+        j = 0
+    if maxi > size_file:
+        maxi = size_line
+    if maxj > size_line:
+        maxj = size_line
+    ind = -1
+    go = False
+    for a in range(i, maxi):
+        ind += 1
+        for b in range(j, maxj):
+            if '0' <= lines[a][b] <= '9':
+                debug = lines[a][b]
+                num = 0
+                for c in range(b-2, b+3):
+                    if c < 0:
+                        c = 0
+                    elif c >= size_line:
+                        break
+                    elif c+2 < b+3 and lines[a][c+1] == '.' and '0' <= lines[a][c+2] <= '9':
+                        continue
+                    else:
+                        if '0' <= lines[a][c] <= '9':
+                            go = True
+                            num += int(lines[a][c])
+                            num *= 10
+                if not stock:
+                    stock.append(num // 10)
+                elif len(stock) == 1 and num//10 != stock[0]:
+                    stock.append(num//10)
+    print(stock)
+    return stock
+
+
+def day3_2(filename):
+    # Not Working !
+    file = open(filename, 'r')
+    lines = file.read().splitlines()
+    size_file = len(lines)
+    size_line = len(lines[0])
+    res = 0
+    for i in range(size_file):
+        j = 0
+        while j < size_line:
+            if lines[i][j] == '*':
+                stock = got_num(lines, i-1, j-1, size_file, size_line)
+                if len(stock) == 2:
+                    res += stock[0] * stock[1]
+            j += 1
+    return res
+
+# print(day3_2("test.txt"))
+# print(day3_2("input.txt"))
